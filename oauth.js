c6fe3eff77011755b6001027483385b7cecb53a0
Jonathon Murphy <jmurphy@jmurphy-mac.local>
2018-11-13 08:38:39 -0500

    cleaning up

---
+++ oauth.js
@@ -0,0 +1,8 @@
+module.exports = {
+  consumer_key:         'fzHfkiNorgQNlTDO5eMT5TQwV',
+  consumer_secret:      'y0OKHVf2QtxiFAUa2N3oNSfsYPcTQcftaDkNqN3i7U6hyAOQ2m',
+  access_token:         '1061985901251907590-Ehruy9LArMqWHSvXIQYlMXZpHLxuts',
+  access_token_secret:  '98sxSM6jZMqWJsRoHlfG20Hp4aufmQayOOY6RfytsA29r',
+  timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
+  strictSSL:            true,     // optional - requires SSL certificates to be valid.
+}
--- tweetQuotes.0.1.js
+++ tweetQuotes.0.1.js
@@ -3,18 +3,13 @@ const util = require('util');
 const path = require('path');
 const fs = require('fs');
 
-const quotesJSON = fs.readFileSync('./quotes.json')
+const quotesJSON = fs.readFileSync('./quotes.json');
 const Mbmbam = JSON.parse(quotesJSON);
-const hastag = ' #MBMBAM'
+const hastag = ' #MBMBAM';
 
-var T = new Twit({
-  consumer_key:         'fzHfkiNorgQNlTDO5eMT5TQwV',
-  consumer_secret:      'y0OKHVf2QtxiFAUa2N3oNSfsYPcTQcftaDkNqN3i7U6hyAOQ2m',
-  access_token:         '1061985901251907590-Ehruy9LArMqWHSvXIQYlMXZpHLxuts',
-  access_token_secret:  '98sxSM6jZMqWJsRoHlfG20Hp4aufmQayOOY6RfytsA29r',
-  timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
-  strictSSL:            true,     // optional - requires SSL certificates to be valid.
-})
+const config = require('./oauth');
+
+let T = new Twit(config);
 
 function tweetIt(){
   let quoteCount = Mbmbam.Quotes.length;
@@ -26,7 +21,7 @@ function tweetIt(){
 }
 setInterval(tweetIt, 86400000);
 
-// let stream = T.stream('user');
+// let stream = T.stream('');
 // stream.on('follow', followed);
 // function followed(eventMsg){
 //   let replyCount = Mbmbam.Replies.length;
