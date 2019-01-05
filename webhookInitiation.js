4150946f08fb29c50c5a86714f946a84a1541d9c
Jonathon Murphy <jmurphy@jmurphy-mac.local>
2019-01-04 22:54:20 -0500

    Update webhooks

--- webhookInitiation.js
+++ webhookInitiation.js
@@ -1,6 +1,11 @@
-var http = require("http");
+const http = require("http");
 
-var options = {
+
+const data = JSON.stringify({
+  todo: 'Buy the milk'
+})
+
+const options = {
     host: "api.twitter.com/",
     path: "account_activity/all/development/webhooks",
     method: "POST",
@@ -8,4 +13,19 @@ var options = {
         "Content-Type": "application/json"
         "Authorization": "Bearer token"
     }
-};
+}
+
+const req = https.request(options, (res) => {
+  console.log(`statusCode: ${res.statusCode}`)
+
+  res.on('data', (d) => {
+    process.stdout.write(d)
+  })
+})
+
+req.on('error', (error) => {
+  console.error(error)
+})
+
+req.write(data)
+req.end()
