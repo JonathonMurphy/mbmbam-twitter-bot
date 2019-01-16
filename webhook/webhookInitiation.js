ed65bfab56bbfc0efcf663c1ba1bf81652262f78
Jonathon Murphy <jmurphy@jmurphy-mac.local>
2019-01-15 19:24:51 -0500

    -_-

--- webhook/webhookInitiation.js
+++ webhook/webhookInitiation.js
@@ -3,7 +3,7 @@ const request = require('request-promise')
 
 const options = {
     host: "api.twitter.com",
-    path: "/1.1/account_activity/all/development/webhooks.json?url=https%3A%2F%2Fjsmurphy.inf%2Fpublic_htmlo%2Fwebhooks%2Ftwitter'",
+    path: "/1.1/account_activity/all/development/webhooks.json?url=http%3A%2F%2Fjsmurphy.info%2Fpublic_html%2Fwebhooks%2Ftwitter",
     method: "POST",
     port: 443,
     uri: "http://jsmurphy.info/webhooks/twitter",
@@ -13,7 +13,7 @@ const options = {
 }
 
 // POST request to create webhook config
-request.post(soptions).then(function(body) {
+request.post(options).then(function(body) {
     console.log(body)
 }).catch(function(body) {
     console.log(body)
