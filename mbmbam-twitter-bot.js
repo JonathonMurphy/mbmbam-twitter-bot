4c3cf5b0fcca0221aff0507a34656072a71c4237
Jonathon Murphy <jsmurphy1991@gmail.com>
2019-11-13 12:05:54 -0500

    removing unnecessary dependencies

--- mbmbam-twitter-bot.js
+++ mbmbam-twitter-bot.js
@@ -9,7 +9,7 @@ const fs = require('fs'),
       config = require('./oauth.js'),
       T = new Twit(config);
 
-const options = {
+const random = {
   uri: 'https://mbmbam.app/api/search/random',
   headers: {
     'User-Agent': 'Request-Promise'
@@ -17,8 +17,16 @@ const options = {
   json: true,
 };
 
+const reply =
+  uri: 'https://mbmbam.app/api/search/reply',
+  headers: {
+    'User-Agent': 'Request-Promise'
+  },
+  json: true,
+};
+
 async function pickTweet() {
-  rp(options)
+  rp(random)
     .then(function(response) {
       let noQuotesFound = 0;
       for (let index of response) {
@@ -51,7 +59,7 @@ function postTweet(tweet) {
 }
 
 function pickReply(user) {
-  let replyPicker = Math.floor(Math.random()*newFollowerReplies.replies.length);
+  // let replyPicker = Math.floor(Math.random()*newFollowerReplies.replies.length);
   let reply = '@'+user + ' ' + newFollowerReplies.replies[replyPicker];
   postTweet(reply);
 }
@@ -64,6 +72,55 @@ function checkForNewFollowers() {
       console.log('No new followers');
     }
   }
+}
+
+let followers = {
+  note: "A user's following status may say 'false' if they were just followed when the script was run." ,
+  users: []
+}
+
+function followUser(screen_name, name) {
+  T.post('friendships/create', { screen_name: screen_name }, function(err) {
+    if (err) {
+      console.log('error:', err);
+    } else {
+      console.log('You are now following ' + name);
+    }
+  })
+}
+
+//Gets current followers info and updates followersInfo.json if there is a new follower
+function getFollowerInfo() {
+  T.get('followers/list', { screen_name: 'Mbmbam_Quotes' },  function (err, data, response) {
+    if (err) {
+      console.log('error:', err);
+      // context.fail();
+    } else {
+      for (i=0; i < data.users.length; i++) {
+        // Checks if bot is not following a user back, and then follows them
+        if (data.users[i].following == false) {
+          followUser(data.users[i].screen_name, data.users[i].name);
+        }
+        // Sets us data structure for logs
+        let userInfo = {
+          id: '',
+          name: '',
+          screen_name: '',
+        }
+        // Sets the info for each follower in the logs
+        userInfo.id = data.users[i].id;
+        userInfo.name = data.users[i].name;
+        userInfo.screen_name = data.users[i].screen_name;
+        userInfo.following = data.users[i].following;
+        followers.users.push(userInfo);
+      }
+      // Writes log file
+      fs.writeFileSync(followerInfoPath, JSON.stringify(followers), function(err) {
+        if(err) console.log(err)
+      })
+
+    }
+  })
 }
 
 pickTweet();
